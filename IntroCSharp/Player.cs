5175d19a286a03093c4f278615cbfe7e754e5384
jpandiscia1709 <jpangithub1709@gmail.com>
2021-10-20 17:38:31 -0400

    initial setup of Player Class

---
+++ IntroCSharp/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroCSharp
+{
+    class Player
+    {
+
+        // member variables
+        public string name;
+        public string gesture;
+        public string ChosenGesture;
+        public int score;
+
+        // constructor
+
+
+        // member methods
+    }
+}
---
+++ IntroCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player human = new Player();
+            Player computer = new Player();
+
+
+            human.name = "Frank";
+            computer.name = "Brainiac";
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(human.name);
+            Console.WriteLine(computer.name);
+            Console.ReadLine();
+        }
+    }
+}
